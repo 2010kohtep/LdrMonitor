0f6828d1d8aeecb368226b11d26b6146bcd2cccd
2010kohtep <sania40000@yandex.ru>
2019-01-16 19:13:27 +0300

    Add C-like types

--- Xander.LdrMonitor.pas
+++ Xander.LdrMonitor.pas
@@ -5,34 +5,41 @@ unit Xander.LdrMonitor;
 interface
 
 type
-  PUnicodeString = ^TUnicodeString;
-  TUnicodeString = packed record
+  PCUNICODE_STRING = ^UNICODE_STRING;
+  UNICODE_STRING = packed record
     Length: Word;
     MaximumLength: Word;
     Buffer: PWideChar;
   end;
+  TUnicodeString = UNICODE_STRING;
+  PUnicodeString = ^TUnicodeString;
 
 type
-  TNotificationReason = (LDR_DLL_NOTIFICATION_REASON_LOADED = 1, LDR_DLL_NOTIFICATION_REASON_UNLOADED = 2);
+  NotificationReasonEnum = (LDR_DLL_NOTIFICATION_REASON_LOADED = 1, LDR_DLL_NOTIFICATION_REASON_UNLOADED = 2);
+  TNotificationReason = NotificationReasonEnum;
 
-  PLdrDLLLoadedNotificationData = ^TLdrDLLLoadedNotificationData;
-  TLdrDLLLoadedNotificationData = record
+  PLDR_DLL_LOADED_NOTIFICATION_DATA = ^LDR_DLL_LOADED_NOTIFICATION_DATA;
+  LDR_DLL_LOADED_NOTIFICATION_DATA = record
     Flags: LongWord;
     FullDllName: PUnicodeString;
     BaseDllName: PUnicodeString;
     DllBase: Pointer;
     SizeOfImage: LongWord;
   end;
+  TLdrDLLLoadedNotificationData = LDR_DLL_LOADED_NOTIFICATION_DATA;
+  PLdrDLLLoadedNotificationData = ^TLdrDLLLoadedNotificationData;
 
   PLdrDLLUnloadedNotificationData = ^TLdrDLLUnloadedNotificationData;
   TLdrDLLUnloadedNotificationData = TLdrDLLLoadedNotificationData;
 
-  PLdrDllNotificationData = ^TLdrDllNotificationData;
-  TLdrDllNotificationData = record
+  PLDR_DLL_NOTIFICATION_DATA = ^LDR_DLL_NOTIFICATION_DATA;
+  LDR_DLL_NOTIFICATION_DATA = record
     case Integer of
       0: (Loaded: TLdrDLLLoadedNotificationData);
       1: (Unloaded: TLdrDLLUnloadedNotificationData);
   end;
+  TLdrDllNotificationData = LDR_DLL_NOTIFICATION_DATA;
+  PLdrDllNotificationData = ^TLdrDllNotificationData;
 
 type
   TLdrRegisterDllNotification = function(Flags: LongWord; NotificationFunction: Pointer; Context: Pointer; Cookie: Pointer): LongWord; stdcall;
